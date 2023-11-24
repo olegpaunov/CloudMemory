de97073aa459dce71d05318b814ae7bac083e8c4
Oleg <gtheplayer2@gmail.com>
2023-11-24 18:30:07 +0200

    FrontEnd

---
+++ client/src/components/App.jsx
@@ -0,0 +1,36 @@
+import React, {useEffect} from 'react';
+import Navbar from "./navbar/Navbar";
+import './app.css'
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import Registration from "./authorization/Registration";
+import Login from "./authorization/Login";
+import {useDispatch, useSelector} from "react-redux";
+import {auth} from "../actions/user";
+
+function App() {
+    const isAuth = useSelector(state => state.user.isAuth)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(auth())
+    }, [dispatch])
+
+
+    return (
+        <BrowserRouter>
+            <div className='app'>
+                <Navbar/>
+                <div className="wrap">
+                    {!isAuth &&
+                    <Routes>
+                        <Route path="/registration" component={Registration}/>
+                        <Route path="/login" component={Login}/>
+                    </Routes>
+                    }
+                </div>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
